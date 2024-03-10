1629ca283b077f4a7873f73b5953ca3c2477eba3
shahad0abd <sh1.abullah@gmail.com>
2024-03-10 20:01:30 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:electech/pages/HomePage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
